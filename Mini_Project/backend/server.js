bc85c5842c3429c56c8365daa897d7fc4e907470
vamsi <vamsikrishnapallam@gmail.com>
2025-12-29 11:43:01 +0530

    added new route to ci=onnect others.

--- Mini_Project/backend/server.js
+++ Mini_Project/backend/server.js
@@ -1,3 +1,4 @@
+const PORT = 5000;
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -34,7 +35,9 @@ io.on("connection", socket => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Backend running on http://0.0.0.0:${PORT}`);
 });
+
+
