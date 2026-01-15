4336b2653d2516201fdaca0ad876de9dbc964608
vamsi <vamsikrishnapallam@gmail.com>
2026-01-15 18:23:02 +0530

    Updated correct database password

--- academic-exchange/backend/config/db.js
+++ academic-exchange/backend/config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 // 👇 WE ARE PASTING THE LINK DIRECTLY HERE TO FORCE IT TO WORK
-const connectionString = "postgresql://academic_db_vwin_user:5QC1JsqgMZWikfbCFcLJ54rayFlKfkuc@dpg-d5kd1u3e5dus73a6lqmg-a/academic_db_vwin";
+const connectionString = "postgresql://academic_db_vwin_user:5QC1JsqgMZWikfbCFcLJ54rayFIKfkuc@dpg-d5kd1u3e5dus73a6lqmg-a/academic_db_vwin";
 
 const pool = new Pool({
     connectionString: connectionString,
